d05d36ab4c5456f236be8f4543592db8a18be10f
TechnoHong <tjrwns51@naver.com>
2022-04-17 14:57:50 +0900

    0417
    Done...
    
    Todo...
     - Workout 기능 추후 추가 (데이터 수집 필요)

--- app/src/main/java/com/tcnhong/hawkout/MainActivity.kt
+++ app/src/main/java/com/tcnhong/hawkout/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.hawkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import com.example.hawkout.databinding.ActivityMainBinding
-import com.tcnhong.hawkout.database.ExerciseDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,24 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.mainStartBtn.setOnClickListener {
-            val intent = Intent(this, WorkoutActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, WorkoutActivity::class.java)
+//            startActivity(intent)
+            Toast.makeText(this, getString(R.string.notyet), Toast.LENGTH_LONG).show();
         }
 
         binding.mainCalculatorBtn.setOnClickListener {
             val intent = Intent(this, CalculatorActivity::class.java)
             startActivity(intent)
         }
-
-        val db = ExerciseDatabase.getInstance(applicationContext)!!
-
-        CoroutineScope(Dispatchers.Main).launch {
-            val exercises = CoroutineScope(Dispatchers.IO).async {
-                db.exerciseDao().getAll()
-            }.await()
-            Log.d("TAG",exercises.toString())
-        }
-
     }
 
     override fun onDestroy() {
